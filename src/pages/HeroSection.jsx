7d001cd06ce78fab5ed70f568013b28c0d8bdac5
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-02-07 20:49:07 +0100

    dodaje linka do ostatniego kongresu

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -115,7 +115,7 @@ export default function HeroSection() {
                             </p>
                             <div className="mt-10 flex items-center gap-x-6">
                                 <a
-                                    href="#"
+                                    href="https://2023.tcmkongres.pl/"
                                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     {t("ostatnia-edycja")} <span aria-hidden="true">→</span>

78d3b15eb99bff0a7b8f587582f438f81dd4bbd7
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-02-07 20:45:38 +0100

    dodaje brakujace tlumaczenie

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -118,7 +118,7 @@ export default function HeroSection() {
                                     href="#"
                                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-                                    Ostania ecyja <span aria-hidden="true">→</span>
+                                    {t("ostatnia-edycja")} <span aria-hidden="true">→</span>
                                 </a>
                                 {/*<a href="#" className="text-sm/6 font-semibold text-gray-900">*/}
                                 {/*    Learn more <span aria-hidden="true">→</span>*/}

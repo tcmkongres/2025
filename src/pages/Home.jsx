586fd2a48fecf413e4e3979b30484816fb949d2f
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-08 23:50:24 +0200

    HOME - wersja przed prod

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,36 @@
 import { useTranslation } from "react-i18next";
 import HeroSection from "./HeroSection.jsx";
+import AboutSection from "./AboutSection.jsx";
+import TcmSection from "./TcmSection.jsx";
+import ImageSection from "./ImageSection.jsx";
+import SpeakersSection from "./SpeakersSection.jsx";
+import PricingSection from "./PricingSection.jsx";
+import QuestionsSection from "./QuestionsSection.jsx";
+import CalenderSection from "./CalenderSection.jsx";
+import HotelSection from "./HotelSection.jsx";
+import SponsorsSection from "./SponsorsSection.jsx";
+import Footer from "./Footer.jsx";
 
 function Home() {
-    const { t } = useTranslation();
-    return (
-        <div>
-            <HeroSection />
-            {/*<AboutSection />*/}
-            {/*<SpeakersSection />*/}
-            {/*<ScheduleSection />*/}
-            {/*<HotelSection />*/}
-            {/*<PastEditionsSection />*/}
-            {/*<TicketsSection />*/}
-            {/*<FAQSection />*/}
-            {/*<OrganizerSection />*/}
-            {/*<MainPartnerSection />*/}
-            {/*<PartnersSection />*/}
-            {/*<ContactSection />*/}
-            {/*<FooterSection />*/}
-        </div>
-    );
-}
+  const { t } = useTranslation();
+  return (
+    <div className="bg-white">
+      <HeroSection />
+      <main className="isolate">
+        <TcmSection />
+        <ImageSection />
+        <SpeakersSection />
+        <CalenderSection />
+        <PricingSection />
+        <QuestionsSection />
+        <HotelSection />
+        <SponsorsSection />
+      </main>
 
-// const HeroSection = () => <div className="h-screen bg-gray-100">Hero</div>;
-const AboutSection = () => <div>O nas</div>;
-const SpeakersSection = () => <div>Wykładowcy</div>;
-const ScheduleSection = () => <div>Program wykładów</div>;
-const HotelSection = () => <div>Hotel</div>;
-const PastEditionsSection = () => <div>Poprzednie edycje</div>;
-const TicketsSection = () => <div>Zakup biletu</div>;
-const FAQSection = () => <div>Pytania</div>;
-const OrganizerSection = () => <div>Organizator</div>;
-const MainPartnerSection = () => <div>Partner główny</div>;
-const PartnersSection = () => <div>Partnerzy</div>;
-const ContactSection = () => <div>Kontakt</div>;
-const FooterSection = () => <div>Stopka</div>;
+      <Footer />
+      {/*<AboutSection />*/}
+    </div>
+  );
+}
 
 export default Home;

3dacc9b38fa22f219c38eeb413de7bb691a1ea97
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-08 23:12:32 +0200

    HotelSection - nowa sekcja

---
+++ src/pages/HotelSection.jsx
@@ -0,0 +1,58 @@
+import hotel from "../assets/hotel1.jpg";
+
+const HotelSection = () => {
+  return (
+    <div className="relative isolate -z-10 mt-32 sm:mt-40">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
+          <img
+            alt=""
+            src={hotel}
+            className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
+          />
+          <div className="w-full flex-auto">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
+              KONGRES VENUE
+            </h2>
+            <p className="mt-6 text-lg/8 text-pretty text-gray-300">
+              Qubus Hotel Kraków znajduje się tuż obok zabytkowej dzielnicy
+              Kazimierz. Usytuowanie hotelu w Krakowie w centrum, nad samą
+              rzeką, stwarza doskonałe warunki do eksplorowania tego
+              przepięknego miasta. Hotel w centrum Krakowa oddaje do dyspozycji
+              Gości także nowoczesne Centrum Fitness z sauną, jacuzzi i
+              siłownią. Atrakcją jest możliwość pływania w przeszklonym basenie
+              z widokiem na Stare Miasto. W ofercie hotelu znajdują się także
+              propozycje muzyczno-gastronomiczne. Oryginalny lobby bar Torino
+              Paninoteka&Bar jest idealnym miejscem na odpoczynek przy koktajlu
+              lub kawie, a kameralny Piano Bar na małą imprezę. Na terenie
+              obiektu znajduje się też płatny parking.
+            </p>
+            <ul
+              role="list"
+              className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base/7 text-white sm:grid-cols-2"
+            ></ul>
+            <div className="mt-10 flex">
+              <a href="#" className="text-sm/6 font-semibold text-indigo-400">
+                See our job postings <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+          }}
+          className="aspect-1318/752 w-[82.375rem] flex-none bg-linear-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HotelSection;

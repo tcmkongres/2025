1a99e5943f59bbf39c01b8d406cb99277dfa59b2
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-08 21:56:30 +0200

    hero key dla tlumaczen

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -15,6 +15,13 @@ const navigation = [
   // { name: 'Company', href: '#' },
 ];
 
+const heroKey = {
+  title: "hero-key-section-title",
+  data: "hero-key-section-data",
+  description: "hero-key-section-description",
+  vol: "hero-key-section-edition",
+};
+
 export default function HeroSection() {
   const { t } = useTranslation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -109,18 +116,15 @@ export default function HeroSection() {
         <div className="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
             <h1 className="max-w-2xl text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">
-              {/*literal*/}
-              {t("tytul-glowny")}
+              {t(heroKey.title)}
             </h1>
             <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
               <p className="text-2xl font-medium text-pretty text-gray-500 sm:text-4xl">
-                {/*literal*/}
-                {t("tytul-data")}
+                {t(heroKey.data)}
               </p>
 
               <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-                {/*literal*/}
-                {t("opis-hero")}
+                {t(heroKey.description)}
               </p>
 
               <div className="mt-10 flex items-center gap-x-6">
@@ -128,8 +132,7 @@ export default function HeroSection() {
                   href="https://2023.tcmkongres.pl/"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  {/*literal*/}
-                  {t("ostatnia-edycja")} <span aria-hidden="true">→</span>
+                  {t(heroKey.vol)} <span aria-hidden="true">→</span>
                 </a>
                 {/*<a href="#" className="text-sm/6 font-semibold text-gray-900">*/}
                 {/*  Learn more <span aria-hidden="true">→</span>*/}
